c32afc91302e85f5f257773b81bb6b5eefceeea9
njitss <vdbergsb@gmail.com>
2020-02-26 20:28:34 +0100

    fixed code even more

--- src/Main.java
+++ src/Main.java
@@ -22,8 +22,8 @@ public class Main {
 
 
     private void run() {
-//        doRandomPerm();
-//        doUsedPerm();
+        doRandomPerm();
+        doUsedPerm();
         doSwapPerm();
     }
 
